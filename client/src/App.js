db4956e7ca013f06e5c197dc82d6d3885537726a
florian lahitte <lahitte.florian@gmail.com>
2019-05-11 00:38:10 -0400

    error email

--- client/src/App.js
+++ client/src/App.js
@@ -6,35 +6,49 @@ class App extends Component {
   state = {
     email: "",
     select: "",
-    error: false,
-    ok: false,
+    errorEmail: false,
+    errorInterest: false,
+    okEmail: false,
+    okSelect: false,
     waitingForServer: false,
     loggedIn: true
   }
-
+  
   login=()=>{
     this.setState({waitingForServer:true})
+    
     if(this.state.email === ""){
       setTimeout(() => {
-        this.setState({error:true});
-        this.setState({ok:false});
+        this.setState({errorEmail:true});
+        this.setState({okEmail:false});
         this.setState({waitingForServer:false})
       }, 2000)
     }
+    else if(this.state.select=== ""){
+      this.setState({errorEmail:false});
+      this.setState({errorInterest:true});
+      this.setState({okSelect:false});
+      this.setState({waitingForServer:false});
+    }
     else{
       setTimeout(() => {
-      this.setState({ok:true})
-      this.setState({error:false})
+      this.setState({okEmail:true});
+      this.setState({okSelect:true});
+      this.setState({errorEmail:false});
       this.setState({waitingForServer:false})
       this.setState({loggedIn:false});
     }, 2000)
     }
+
+    
+  
     console.log("Email:" + this.state.email);
     console.log("Interest:" + this.state.select);
   }
 
   handleType=(event)=>{
     this.setState({[event.target.name]: event.target.value})
+    console.log(this.state.email)
   }
   render() {    
     return (
@@ -42,13 +56,14 @@ class App extends Component {
         <h2 className="title">INTERNSHIP SIGN UP FORM</h2>
         <hr></hr>
         {this.state.loggedIn&&<div><p className="text-paragraphe">Prepare for your career with Project Manager Management, Web-Developement, Graphic design, or Digital Marketing Internship at Northern</p>
+          {this.state.errorInterest&&<span>Please select an Interest</span>}
           <div className="row">
             <div className="col">
-            {this.state.error&&<span>Please enter a valid email address</span>}
-              <input  className={"form-input " +(this.state.error&&"form-error")} disabled={this.state.waitingForServer} onChange={this.handleType} name="email" type="email" placeholder="Your Email Address *"/>
+            {this.state.errorEmail&&<span>Please enter a valid email address</span>}
+              <input  className={`form-input " ${this.state.errorEmail&&"form-error"}`} disabled={this.state.waitingForServer} onChange={this.handleType} name="email" type="email" placeholder="Your Email Address *"/>
             </div>
             <div className="col">
-              <select className="form-input myselect" disabled={this.state.waitingForServer} name="select" onChange={this.handleType}>
+              <select className={`form-input myselect " ${this.state.errorInterest&&"form-error"}`} disabled={this.state.waitingForServer} name="select" onChange={this.handleType}>
                 <option value="" default>Your Interests</option>
                 <option value="Audi">Audi</option>
                 <option value="BMW">BMW</option>
@@ -57,7 +72,7 @@ class App extends Component {
             </div>
             <button className="button-submit" onClick={this.login}>{this.state.waitingForServer?"Submitting...":"Sign Up Now"}</button>
         </div>}
-          {this.state.ok&&<Good/>}
+          {this.state.okSelect&&this.state.okEmail&&<Good/>}
       </div>
     );
   }
